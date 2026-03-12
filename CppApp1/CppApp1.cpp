4f906143c4f6ab640ca4bc9366bf8cefe3072f18
4B2G0924 <a0968670693@gmail.com>
2026-03-12 14:44:53 +0800

    123

--- CppApp1/CppApp1.cpp
+++ CppApp1/CppApp1.cpp
@@ -1,20 +1,32 @@
-﻿// CppApp1.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
-//
+﻿#include <iostream>
+#include <string>
 
-#include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    string str1 = "Hello";
+    string str2 = "World";
+    string str3 = str1 + " " + str2 + "!";
+    string str3a = "\"Hello \\ World!\"";
+    auto str3b = R"("Hello \ world!")";
+
+    // 輸出字串內容
+    cout << "str3: " << str3 << endl;
+    cout << "str3a: " << str3a << endl;
+    cout << "str3b: " << str3b << endl;
+
+    cout << endl; // 換行隔開
+
+    cout << "str3 capacity = " << str3.capacity() << endl;
+    cout << "str3 length(size) = " << str3.length() << endl;
+    cout << "str3 max_size = " << str3.max_size() << endl;
+    cout << "str3 size = " << str3.size() << endl;
+    cout << "str3 empty = " << str3.empty() << endl; // 0 代表 false (不為空)
+
+    // 字元存取
+    cout << "str3[0] = " << str3[0] << endl;   // 取得第一個字元 'H'
+    cout << "str3.at(4) = " << str3.at(4) << endl; // 取得索引 4 的字元 'o'
+    cout << "str3.front() = " << str3.front() << endl; // 取得字串開頭 'H'
+
+    return 0;
 }
-
-// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
-// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
-
-// 開始使用的提示: 
-//   1. 使用 [方案總管] 視窗，新增/管理檔案
-//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
-//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
-//   4. 使用 [錯誤清單] 視窗，檢視錯誤
-//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
-//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
